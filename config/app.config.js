07d26b5bee2d0e6a36c5670a9e648e0b923d9636
Fuyi Atlas <thread.zhou@gmail.com>
2023-11-22 21:28:30 +0800

    feat: project init(hello world)

---
+++ config/app.config.js
@@ -0,0 +1,12 @@
+export const APP_CONFIG = {
+  base_url: 'http://127.0.0.1:18080/v1/',
+  apis: {
+      oauth: {
+          we_chat: 'oauth/we_chat',
+      },
+  },
+  constants: {
+      auth_token: 'auth_token',
+      followed_cities: 'followed_cities'
+  }
+}
---
+++ request/index.js
@@ -0,0 +1,30 @@
+import { APP_CONFIG } from '../config/appConfig'
+
+export const request = (params) => {
+    let {url} = params;
+    if(!String(url).startsWith('https')){
+        if(String(url).startsWith('/')){
+            url = String(url).substring(1, String(url).length)
+        }
+        url = APP_CONFIG.base_url + url
+    }
+    return new Promise((resolve, reject) => {
+        wx.request({
+            ...params,
+            header: {
+                'Authorization': wx.getStorageSync(APP_CONFIG.constants.auth_token)
+            },
+            url,
+            success: (response) => {
+                if(response.data.code === 200){
+                    resolve(response.data)
+                }else {
+                    reject(response.data)
+                }
+            },
+            fail: (err) => {
+                reject(err)
+            }
+        })
+    })
+}
